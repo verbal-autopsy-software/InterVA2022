b87c5fcf868fbf48af5d9369413ac03262af2094
jarathomas <jaratthomas@gmail.com>
2025-05-03 17:47:29 -0400

    renamed package to include Prerelease

---
+++ InterVA2022/R/InterVA2022-package.R
@@ -0,0 +1,54 @@
+
+#' Conditional probability of the 2022 Probbase
+#'
+#' This is the table of conditional probabilities of symptoms given CODs, along with
+#' prior probabilities in the first row. The values correspond
+#' to the WHO VA 2022 instrument.
+#'
+#'
+#' @name probbase2022
+#' @docType data
+#' @format A data frame with 343 observations on 71 variables. 
+#' @keywords datasets
+#' @examples
+#' 
+#' data(probbase2020)
+#'
+NULL
+
+
+#' Causes for the WHO 2022 instrument
+#'
+#' This is the table of VA causes and their codes for the WHO 2022 instrument.
+#'
+#'
+#' @name causetext2022
+#' @docType data
+#' @format A data frame with 67 rows and 3 columns.
+#' @keywords datasets
+#' @examples
+#' 
+#' data(causetext2022)
+#'
+NULL
+
+
+
+
+#' 23 records of Sample Input
+#'
+#' This is a dataset consisting of 23 arbitrary sample input deaths in the
+#' acceptable format of the WHO2022 data format. Any dataset that needs to be analyzed by this
+#' package should be in the same format. The order of the input fields must
+#' not be changed.
+#'
+#'
+#' @name RandomVA2022
+#' @docType data
+#' @format 23 arbitrary input records.
+#' @keywords datasets
+#' @examples
+#'
+#' data(RandomVA2022)
+#'
+NULL
--- README.md
+++ README.md
@@ -1,3 +1,14 @@
-# InterVA2022
+**Pre-release Test version** of InterVA for the WHO 2022 VA instrument. 
 
-InterVA model for the 2022 WHO VA instrument.
+
+* <ins>**_Use at your own risk: we do not suggest or guarantee useful operation of any kind._**</ins>
+* <ins>**_The package has not been tested or validated in any way._**</ins>
+* <ins>**_At this time, we do not support this package in any way._**</ins>
+
+Install and load the package with the following commands:
+
+```
+library(devtools)
+install_github("verbal-autopsy-software/InterVA2022", subdir = "InterVA2022")
+library(InterVA2022Prerelease)
+```
